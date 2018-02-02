007dfea546456fc9d9751483c94733c352444cc0
Roy <roykid123@gmail.com>
2018-02-02 16:06:22 +0200

    fdggdg

--- simulator.py
+++ simulator.py
@@ -15,25 +15,28 @@ with DBCon:
         if cursor.fetchone()[0]==0:
             break
         cursor.execute("SELECT * FROM tasks")
-        for task in cursor.fetchall():
+        tasks=cursor.fetchall()
+        for task in tasks:
             cursor.execute("SELECT status, name FROM workers WHERE id=(?)", (task[2],))
             worker=cursor.fetchone()
             if worker[0]=="idle":
                 currentTask[task[2]]=task[0]
                 cursor.execute("UPDATE workers SET status=(?) WHERE id=(?)", ("busy", task[2]))
                 cursor.execute("UPDATE resources SET amount=amount-(?) WHERE name=(?)", (task[5], task[4]))
+                cursor.execute("UPDATE tasks SET time_to_make=time_to_make-1 WHERE id=(?)", (task[0],))
                 print("{} says: work work".format(worker[1]))
 
             # checks if the requested worker of this task is busy on this task
             elif currentTask[task[2]]==task[0]:
-                cursor.execute("UPDATE tasks SET time_to_make=time_to_make-1 WHERE id=(?)", (task[0],))
-                if task[3]>=1:
+                if task[3]>=0:
+                    cursor.execute("UPDATE tasks SET time_to_make=time_to_make-1 WHERE id=(?)", (task[0],))
                     print("{} is busy {}...".format(worker[1], task[1]))
                     # continue
-
+        for task in tasks:
             # checks if time to make of this task=0 and if the requested worker works on this task
-            if task[3]==1 and currentTask[task[2]]==task[0]:
+            if task[3]==0: # and currentTask[task[2]]==task[0]:
                 cursor.execute("DELETE FROM tasks WHERE id=(?)", (task[0],))
                 cursor.execute("UPDATE workers SET status=(?) WHERE id=(?)", ("idle", task[2]))
                 currentTask[task[2]]=-1
-                print("{} says: All Done!".format(worker[1]))
+                cursor.execute("SELECT name FROM workers WHERE id=(?)", (task[2],))
+                print("{} says: All Done!".format(cursor.fetchone()[0]))
