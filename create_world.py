672f7c42cd383e1fad87d655f0cb6e9fe6321df4
Roy <roykid123@gmail.com>
2018-02-03 21:04:56 +0200

    עיביע

--- create_world.py
+++ create_world.py
@@ -6,9 +6,8 @@ isDBExist=os.path.isfile("world.db")
 DBCon=sqlite3.connect("world.db")
 
 with DBCon:
-    cursor=DBCon.cursor()
     if not isDBExist:  # First time creating the database. Create the tables
-        cursor.execute("CREATE TABLE tasks("
+        DBCon.execute("CREATE TABLE tasks("
                        "ID INTEGER PRIMARY KEY,"
                        "task_name TEXT NOT NULL,"
                        "worker_id INTEGER REFERENCES workers(id),"
@@ -16,12 +15,12 @@ with DBCon:
                        "resource_name TEXT NOT NULL,"
                        "resource_amount INTEGER NOT NULL)")
 
-        cursor.execute("CREATE TABLE workers("
+        DBCon.execute("CREATE TABLE workers("
                        "id INTEGER PRIMARY KEY,"
                        "name TEXT NOT NULL,"
                        "status TEXT NOT NULL)")
 
-        cursor.execute("CREATE TABLE resources("
+        DBCon.execute("CREATE TABLE resources("
                        "name TEXT PRIMARY KEY,"
                        "amount INTEGER NOT NULL)")
 
@@ -30,10 +29,10 @@ with DBCon:
             for line in config:
                 myList=line.strip().split(",")
                 if len(myList)==2:
-                    cursor.execute("INSERT INTO resources VALUES(?, ?)", (myList[0], myList[1]))
+                    DBCon.execute("INSERT INTO resources VALUES(?, ?)", (myList[0], myList[1]))
                 elif len(myList)==3:
-                    cursor.execute("INSERT INTO workers VALUES(?, ?, ?)", (myList[1], myList[2], "idle"))
+                    DBCon.execute("INSERT INTO workers VALUES(?, ?, ?)", (myList[1], myList[2], "idle"))
                 else:
-                    cursor.execute("INSERT INTO tasks VALUES(?, ?, ?, ?, ?, ?)",
+                    DBCon.execute("INSERT INTO tasks VALUES(?, ?, ?, ?, ?, ?)",
                                    (count, myList[0], myList[1], myList[4], myList[2], myList[3]))
                     count+=1
--- simulator.py
+++ simulator.py
@@ -21,20 +21,20 @@ with DBCon:
             worker=cursor.fetchone()
             if worker[0]=="idle":
                 currentTask[task[2]]=task[0]
-                cursor.execute("UPDATE workers SET status=(?) WHERE id=(?)", ("busy", task[2]))
-                cursor.execute("UPDATE resources SET amount=amount-(?) WHERE name=(?)", (task[5], task[4]))
-                cursor.execute("UPDATE tasks SET time_to_make=time_to_make-1 WHERE id=(?)", (task[0],))
+                DBCon.execute("UPDATE workers SET status=(?) WHERE id=(?)", ("busy", task[2]))
+                DBCon.execute("UPDATE resources SET amount=amount-(?) WHERE name=(?)", (task[5], task[4]))
+                DBCon.execute("UPDATE tasks SET time_to_make=time_to_make-1 WHERE id=(?)", (task[0],))
                 print("{} says: work work".format(worker[1]))
 
             # checks if the requested worker of this task is busy on this task
             elif currentTask[task[2]]==task[0]:
                 if task[3]>=0:
-                    cursor.execute("UPDATE tasks SET time_to_make=time_to_make-1 WHERE id=(?)", (task[0],))
+                    DBCon.execute("UPDATE tasks SET time_to_make=time_to_make-1 WHERE id=(?)", (task[0],))
                     print("{} is busy {}...".format(worker[1], task[1]))
         for task in tasks:
             if task[3]==0:
-                cursor.execute("DELETE FROM tasks WHERE id=(?)", (task[0],))
-                cursor.execute("UPDATE workers SET status=(?) WHERE id=(?)", ("idle", task[2]))
+                DBCon.execute("DELETE FROM tasks WHERE id=(?)", (task[0],))
+                DBCon.execute("UPDATE workers SET status=(?) WHERE id=(?)", ("idle", task[2]))
                 currentTask[task[2]]=-1
                 cursor.execute("SELECT name FROM workers WHERE id=(?)", (task[2],))
                 print("{} says: All Done!".format(cursor.fetchone()[0]))
